e4b9d0a22983459e8431ed8f89f3fa236eb635a6
Muhamad Rizki Hasan <muhamadrizkihasan@smkwikrama.sch.id>
2023-09-13 10:39:19 +0700

    Menambahkan Folder multiDimensiArrayBuiltin

---
+++ 6.multiDimensiArrayBuiltin/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // array multidimensi
+    // array[baris][kolom]
+    int arrayMD[2][2] = {1,2,3,4,};
+
+    cout << arrayMD[0][0] << " " << arrayMD[0][1]<< endl;
+    cout << arrayMD[1][0] << " " << arrayMD[1][1]<< endl;
+    cin.get();
+    return 0;
+}
