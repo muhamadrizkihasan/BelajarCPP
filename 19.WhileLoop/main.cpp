2d335da8768d410d2de5975826a836f05a9c15a8
Muhamad Rizki Hasan <muhamadrizkihasan@smkwikrama.sch.id>
2023-09-18 21:33:36 +0700

    Menambahkan Folder WhileLoop

---
+++ 19.WhileLoop/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    while (a < 10)
+    {
+        cout << "Hore ";
+        cout << a << endl;
+        a += 1;
+    }
+    cout << "Selesai";
+
+    cin.get();
+    return 0;
+}
