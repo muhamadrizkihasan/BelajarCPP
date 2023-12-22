151dbb33546bcfbdccf2d73e14bf642400a4c552
unknown <muhamadrizkihasan@smkwikrama.sch.id>
2023-12-22 23:57:19 +0700

    Menambahkan folder Preprocessing Directive Pendahuluan

---
+++ 72.PreprocessingDirectivePendahuluan/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// macro
+#define PI 3.141592
+using namespace std;
+
+double pi = 3.141592;
+int main(){
+    cout << "Nilai PI = " << PI << endl;
+    cout << "Nilai pi double = " << pi << endl;
+    cout << "Address pi double = " << &pi << endl;
+}
