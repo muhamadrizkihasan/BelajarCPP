b1f37a0e2f58f31bbedb1dc78c0a57344ce65613
unknown <muhamadrizkihasan@smkwikrama.sch.id>
2023-12-22 23:59:03 +0700

    Menambahkan folder Preprocessing Directive include

---
+++ 75  Preprocessing Directive include/library.h
@@ -0,0 +1,7 @@
+#include <string>
+
+#define PI 3.141572
+#define KUADRAT(X) (X*X)
+#define MAX(A,B) ((A>B) ? A:B)
+
+std::string data = "STRING";
---
+++ 75  Preprocessing Directive include/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "library.h"
+
+using namespace std;
+int main(){
+    cout << "Berhasil" << endl << endl;
+    cout << "Nilai PI: " << PI << endl;
+    cout << "Kuadrat: " << KUADRAT(15) << endl;
+    cout << "Max: " << MAX(15,10) << endl;
+}
