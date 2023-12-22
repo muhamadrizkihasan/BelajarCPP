9b203f66ccbb9b09186ba477ac2698c843c58b38
unknown <muhamadrizkihasan@smkwikrama.sch.id>
2023-12-22 23:59:31 +0700

    Menambahkan folder Preprocessing Directive ifdef ifndef pragma

---
+++ 76  Preprocessing Directive ifdef ifndef pragma/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "library.h"
+#include <string>
+
+#define PI 3.14159265359
+using namespace std;
+
+int main(){
+    std::string data = "STRING";
+
+	cout << "berhasil" << endl;
+
+	cout << "PI : " << PI << endl;
+	cout << "KUADRAT: " << KUADRAT(5) << endl;
+	cout << "MAX: " << MAX(10,2) << endl;
+	cout << "data string: " << data << endl;
+	cin.get();
+	return 0;
+}
